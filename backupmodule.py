8995276f9379a2caa7c62f0777aab02b495a7eb4
gherkin21 <repcak210502@gmail.com>
2021-05-19 09:28:28 +0200

    .

--- backupmodule.py
+++ backupmodule.py
@@ -15,25 +15,11 @@ def get_args():
 
 def upload_blob(bucket_name, source_file_name, prefix):
     """Uploads a file to the bucket."""
-    # The ID of your GCS bucket
-    # bucket_name = "your-bucket-name"
-    # The path to your file to upload
-    # source_file_name = "local/path/to/file"
-    # The ID of your GCS object
-    # destination_blob_name = "storage-object-name"
 
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     destination_blob_name = path.basename(source_file_name) if prefix is None else (prefix+"/"+path.basename(source_file_name)).replace("//","/")
-
-    # if prefix is None:
-    #     destination_blob_name = path.basename(source_file_name)
-    # else:
-    #     destination_name = prefix+"/"+path.basename(source_file_name)
-    #     destination_blob_name = destination_name.replace("//","/")
-
     blob = bucket.blob(destination_blob_name)
-
     blob.upload_from_filename(source_file_name)
 
     print(
--- executemodule.py
+++ executemodule.py
@@ -2,5 +2,4 @@ import backupmodule
 
 if __name__ == '__main__':
     args = backupmodule.get_args()
-    # upload_blob(args.bucket, args.file, args.prefix)
-    backupmodule.upload_blob(args.bucket, args.file, "qwerty")
+    backupmodule.upload_blob(args.bucket, args.file, args.prefix)
